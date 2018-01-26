dedf9c9c5a920b4f5dfe408b9c30387ba2be0009
HOME\Andry <shapovala@ukr.net>
2018-01-26 13:35:41 +0200

    changes

---
+++ nekrasof.glyanec.net/mob.js
@@ -0,0 +1,13 @@
+(function ($) {
+	$(document).ready(function() {
+
+		// adaptive menu
+        $('.header .region-header .block_system_main_menu .block_content > .menu,.header .region-header .block_content > .block_menu_header > .menu').slicknav({
+            label: ''
+        });
+
+
+	   
+
+	});
+})(jQuery);
